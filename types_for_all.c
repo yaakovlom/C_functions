d0ebeba560bcd30c02192497659d81636eb8aa4b
Mordechai Greenwald <79059545+yaakovlom@users.noreply.github.com>
2022-03-21 15:28:07 +0200

    added types for all

---
+++ types_for_all.c
@@ -0,0 +1,29 @@
+typedef enum {
+	Sunday = 1,
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday
+} WeekDays;
+
+typedef enum {
+	January = 1, 
+	February, 
+	March, 
+	April, 
+	May, 
+	June, 
+	July, 
+	August, 
+	September, 
+	October, 
+	November, 
+	December
+} Months;
+
+typedef enum {
+	False,
+	True
+} Bool;
